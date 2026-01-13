30d4ad4cfe8dd00532d11d2de1c79b670129db1c
annikagomez <algomez@ldeo.columbia.edu>
2026-01-13 11:58:54 -0500

    Updated notebooks with correct filepaths

--- deseq.R
+++ deseq.R
@@ -1,7 +1,10 @@
 library(DESeq2)
 
+##Set WD to source file location
+setwd("INSERTFILEPATH")
+
 #read in raw counts of t. erythraeum transcripts
-rawCounts <- read.csv("tery_raw_gene_counts.csv",header=TRUE)
+rawCounts <- read.csv("metaT_files/tery_raw_gene_counts.csv",header=TRUE)
 head(rawCounts)
 geneID <- rawCounts$gene
 sampleIndex <- grepl("SS0\\d+",colnames(rawCounts))
@@ -9,7 +12,7 @@ rawCounts <- round(as.matrix(rawCounts[,sampleIndex]))
 rownames(rawCounts) <- geneID
 head(rawCounts)
 
-metaData <- read.csv("slick_metadata.csv",header=TRUE)
+metaData <- read.csv("metaT_files/slick_metadata.csv",header=TRUE)
 head(metaData)
 rownames(metaData) <- metaData$id
 metaData$id <- factor(metaData$id)
@@ -42,6 +45,6 @@ head(deseq2ResDF)
 # Set a boolean column for significance
 deseq2ResDF$significant <- ifelse(deseq2ResDF$padj < .05, "Significant", NA)
 
-write.csv(deseq2ResDF, "t_e_deseq_resdf.csv")
+write.csv(deseq2ResDF, "metaT_files/t_e_deseq_resdf.csv")
 
 
